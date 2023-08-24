c1032b15d02d39123bbdb7bbaba8f684fcc9d6ba
PangHun <112803923+hunirin@users.noreply.github.com>
2023-08-24 09:50:10 +0900

    Revert "CLTK-33 : Issue 배너 구현"

--- src/main/java/team/closetalk/issue/repository/IssueArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package team.closetalk.issue.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.closetalk.issue.entity.IssueArticleEntity;
-
-public interface IssueArticleRepository extends JpaRepository<IssueArticleEntity, Long> {
-}
